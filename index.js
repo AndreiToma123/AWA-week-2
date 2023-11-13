0948902928059bbdea73f1e1411ab7d61b88d658
AndreiToma123 <Andrei.Toma@student.lut.fi>
2023-11-13 23:25:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
-    res.json({
-        msg: 'Hello world'
-    });
-});
+app.get('/hello', (req, res) => {res.json({msg: 'Hello world'});});
 
-app.get('/echo/:id', (req, res) => {
-    const id = req.params.id;
-    res.json({
-        id: id
-    });
-});
+app.get('/echo/:id', (req, res) => {const id = req.params.id;res.json({id: id});});
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+app.listen(port, () => {console.log(`Server listening on port ${port}`);})
