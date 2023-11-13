57f6847daeefdfbe9404198cba8e8e16fa8760f0
AndreiToma123 <Andrei.Toma@student.lut.fi>
2023-11-13 23:24:18 +0200

    .

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+    res.json({
+        msg: 'Hello world'
+    });
+});
+
+app.get('/echo/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({
+        id: id
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
